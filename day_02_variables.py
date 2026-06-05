ea354b8f50d308cb12bc300b58663c392d4bde0f
Aishwarya Samdurle <samdurleaishwarya@gmail.com>
2026-06-05 10:02:06 +0530

    Create day_02_variables.py

---
+++ day_02_variables.py
@@ -0,0 +1,19 @@
+# Day 2 - Python Variables Practice
+# Topic: Variables and Print Statement
+
+# Variables तयार करणे
+student_name = "Aishwarya"
+course_name = "B.Tech AIDS"
+day_number = 2
+subject_1 = "Python"
+subject_2 = "DSA"
+hours_studied = 1.5
+
+# Print करणे
+print("Student Name:", student_name)
+print("Course:", course_name)
+print("Challenge Day:", day_number)
+print("Subject 1:", subject_1)
+print("Subject 2:", subject_2)
+print("Hours Studied Today:", hours_studied)
+print("Day 2 Completed Successfully")
